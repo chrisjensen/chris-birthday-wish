923d409ec4f50abd741f4c7f88eb3f1c17a070b3
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-17 11:25:19 +1200

    More accurate CTA on highest donor

--- components/highest-donor/highest-donor.js
+++ components/highest-donor/highest-donor.js
@@ -1,10 +1,9 @@
 (RaiselyComponents, React) => {
 	const { useEffect, useState } = React;
-	const { get } = RaiselyComponents.Common;
 
 	return function HighestDonor() {
 		const [leadDonor, setLeadDonor] = useState('________');
-		const [leadGap, setLeadGap] = useState(5);
+		const [highestGift, setHighestGift] = useState(5);
 
 		useEffect(() => {
 			async function fetchData() {
@@ -15,10 +14,10 @@
 					throw new Error('Could not load. Try refreshing the page');
 				}
 				const body = await response.json();
-				const [topDonor, secondDonor] = body.data.donors;
+				const [topDonor] = body.data.donors;
 				if (topDonor) {
 					setLeadDonor(topDonor.preferredName);
-					setLeadGap(topDonor.total - getComputedStyle(secondDonor, 'total', 0));
+					setHighestGift(topDonor.total);
 				}
 			}
 			fetchData()
@@ -28,7 +27,7 @@
 		return (
 			<div className="highest-donor__wrapper">
 				<p className="highest-donor__body">Chris will dance to a song chosen by {leadDonor}</p>
-				<p className="highest-donor__gap">(or you if you donate ${leadGap})</p>
+				<p className="highest-donor__gap">(or you if you donate ${highestGift + 1} or more)</p>
 			</div>
 		);
 	}
